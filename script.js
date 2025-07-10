99eb48f00ec0e165e562e8a153ae8c33dd0647af
sky9891 <akash.rai@iitg.ac.in>
2025-07-10 11:57:03 +0530

    Updated landing page

---
+++ script.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+});
+
+document.querySelectorAll(".fade-in").forEach((section) => {
+  observer.observe(section);
+});
